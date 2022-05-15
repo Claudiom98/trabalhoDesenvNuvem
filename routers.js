d979f1f7ba115a145a6b00859a6b7749eceb6121
Glauber Fernandes <glauber.fernandes@hapvida.com.br>
2022-05-15 01:05:51 -0300

    Adicionado a rota areaquadrado e utilizando query params

--- routers.js
+++ routers.js
@@ -14,7 +14,13 @@ router.get("/user", function(req, res){
 });
 
 router.get("/user/:id", function(req, res){
-    res.send("Edita o usuário " + req.params.id)
+    res.send(`Edita o usuário ${req.params.id}`)
+})
+
+router.get("/areaquadrado", function(req, res){
+    let base = req.query.base;
+    let altura = req.query.altura;
+    res.send(`A área do quadrado é: ${base * altura}`)
 })
 
 module.exports = router;
