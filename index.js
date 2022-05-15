2771965dca68bff41ce19402172a1c5a626236d1
Glauber Fernandes <glauber.fernandes@hapvida.com.br>
2022-05-14 23:08:01 -0300

    Criando o arquivo de rotas e adicionado no servidor

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+var routers = require('./routers');
+app.use('/', routers)
+
 app.listen(4000, function(erro){
     if(erro){
         console.log("Erro ao iniciar servidor...");
