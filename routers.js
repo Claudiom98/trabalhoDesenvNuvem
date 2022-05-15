27582ebb223ca50216acc96ccb32355a2499e35d
Glauber Fernandes <glauber.fernandes@hapvida.com.br>
2022-05-15 00:25:55 -0300

    Adicionado a rota /users

--- routers.js
+++ routers.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 router.get("/", function(req, res){
     res.sendFile(__dirname + "/src/index.html");
-})
+});
+
+router.get("/users", function(req, res){
+    res.send("Listar usuários")
+});
 
 module.exports = router;
