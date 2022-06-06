728e48acc7ca161bdc544a78a5742c38e5101bdd
Claudiom98 <claudiom@edu.unifor.br>
2022-06-05 22:56:15 -0300

    mudando para porta http

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const app = express();
 var routers = require('./routers');
 app.use('/', routers)
 
+var port = process.env.PORT || 8080;
+
 app.use(express.static(__dirname + '/src'));
 
-app.listen(4000, function(erro){
+app.listen(port, function(erro){
     if(erro){
         console.log("Erro ao iniciar servidor...");
     } else {
